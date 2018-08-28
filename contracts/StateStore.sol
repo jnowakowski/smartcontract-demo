c5e3f1f3146b421f91a878630a738fb09b12e2ea
Unknown <gierlak@gmail.com>
2018-08-28 11:13:47 +0200

    added active function

--- contracts/StateStore.sol
+++ contracts/StateStore.sol
@@ -9,9 +9,14 @@ contract StateStore
         uint state;
     }
 
+    bool active;
     address[] usersLog;
     mapping(address => User) userStates;
  
+    constructor() public{
+        active = false;
+    }
+
     function saveUserState(string _name, uint _state) external{
         bytes memory name = bytes(_name);
         require(name.length > 0, "name cannot be empty.");
@@ -28,5 +33,13 @@ contract StateStore
 
     function readUsersLog() external view returns (address[] _addr){
         return usersLog;
+    }
+
+    function isActive() external view returns(bool){
+        return active;
+    }
+
+    function setActive(bool _flag) external{
+        active = _flag;
     }
  }
