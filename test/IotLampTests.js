63e6f79def65f53dbbac9b5ab5f13c3973695764
Unknown <gierlak@gmail.com>
2018-08-29 12:50:50 +0200

    added additonal tests

--- test/IotLampTests.js
+++ test/IotLampTests.js
@@ -4,12 +4,11 @@ const assert = require("chai").assert;
 
 contract('IOTLamp', function(accounts) {  
   it("readUsersLog", function() {
-    return IOT.deployed().then(async function(iot) {
-
-      let result = await iot.readUsersLog();
+    return IOT.deployed().then(async function(iot) {      
+      await iot.addUser("Marcin", "user@gmail.com", {from: web3.eth.accounts[1], gas:230000});
+      let result = await iot.getAccessList(); 
       console.log(result);
-      assert.isEmpty(result);
-
+      assert.isNotEmpty(result);
     });
   })
 });
